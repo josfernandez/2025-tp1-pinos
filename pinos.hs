0a70fa9289743024accf3a02741502445ead4792
josfernandez <103599049+josfernandez@users.noreply.github.com>
2025-04-08 10:58:48 -0300

    error en guardas

--- pinos.hs
+++ pinos.hs
@@ -2,8 +2,8 @@ module Library where
 import PdePreludat
 pesoPino :: Number -> Number
 pesoPino altura 
-				  altura<=3 | altura*300
-				  otherwise | 900 +((altura-3)*200)
+| altura<=3 = altura*300
+| otherwise = 900 +((altura-3)*200)
 
 esPesoUtil :: Number -> bool
 esPesoUtil peso = peso>=400 && peso<=1000
